aa3b57f5530ba0eb46c401052c0fc35c137279c4
Yair Batito <yairbe12@gmail.com>
2024-05-08 13:47:36 +0300

    updated version

--- ring.h
+++ ring.h
@@ -2,30 +2,16 @@
 
 #include "std_lib_facilities.h"
 
-class ring_overflow {};
-class ring_underflow{};
 class index_out_of_bounds_exception {};
+using namespace std;
 
-template<class T, int N>
-class ring_it {
-    T *elements;
-    int i;
-public:
-ring_it(T *elements, int i) : elements(elements), i(i) {}
-T operator*() { return elements[i]; }
-bool operator!=(const ring_it &o) { return i != o.i; }
-void operator++() {
-    if(i== N-1 ) i=0;
-    else ++i; }
-
-};
 
 template<class T, int N>
 class ring {
-    int head ,tail , size ;
+    int currIndex;
     T *elements;
  public:
-    ring() :elements(new T[N]), head(0), tail(-1),size(0){
+    ring() :elements(new T[N]), currIndex(0){
     }
 
     T& operator [](int index){
@@ -33,37 +19,28 @@ class ring {
             throw index_out_of_bounds_exception();
         return elements[index];
     }
-    ring_it<T,N> begin(){ return ring_it<T,N> (elements,head) ;  }
-    ring_it<T,N> end(){if(tail == N - 1) tail = -1; return ring_it<T,N> (elements,tail + 1) ; }
-   ~ring(){delete [] elements ;} 
+   ~ring(){
+            std::cout << "call the ~ring" << endl;
+            delete [] elements ;
+        } 
 
     void push_back( T data ){
-        if(size != N ){
-            if(tail == N - 1) tail = -1;
-            elements[++tail] = data;
-            size++;
-            return;
-        }
-        throw ring_overflow();
+        std::cout << "before update this->currIndex = " << this->currIndex << endl;
+        std::cout << "insert " << data << " in pos " << this->currIndex << endl;
+        elements[this->currIndex] = data;
+        this->currIndex = (this->currIndex + 1) % N; // Update index properly
+        std::cout << "after update this->currIndex = " << this->currIndex << endl;
     }
 
-    T& pop_front(){
-        if(size != 0){
-            size--;
-            if(head == N-1){
-                head = 0;
-                return elements[N-1];
-            }
-            return elements[head++];
-        }
-        throw ring_underflow();
-    }
 
 
 };
 template<class T, int N >
 ostream& operator<<(ostream& os , ring <T,N>& x ){
-        for(auto it = x.begin() ; it != x.end() ; ++it)
-            os<< *it << " ";
-        return os;
+    // std::cout << "reading from the list" << endl;
+    os << "[";
+    for(int i = 0; i< N; i++)
+        os<< x[i] << ", ";
+    os << "]";
+    return os;
 };
--- server.cpp
+++ server.cpp
@@ -1,3 +1,4 @@
+#include "ring.h"
 #include "graph.h"
 #include <arpa/inet.h>
 #include <stdlib.h>
@@ -6,12 +7,115 @@
 #include <string> 
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <mutex>
+#include <thread>
 using namespace std;
+#define RING_SIZE 10
 
+
+bool startsWith(const std::string& str, const std::string& prefix) {
+    if (str.length() < prefix.length()) {
+        return false;
+    }
+    return str.compare(0, prefix.length(), prefix) == 0;
+}
+
+bool endsWith(const std::string& str, const std::string& suffix) {
+    if (str.length() < suffix.length()) {
+        return false;
+    }
+    return str.compare(str.length() - suffix.length(), suffix.length(), suffix) == 0;
+}
+
+
+int contains(ring<std::string, RING_SIZE>& history, int v, int u) {
+    for (int i = 0; i < 10; i++) {
+        if (startsWith(history[i], std::to_string(v) + " ") && endsWith(history[i], " " +std::to_string(u) + "\n")) {
+            return i; // Added semicolon
+        }
+    }
+    return -1;
+}
+
+void handleRequest(int fd2, Graph& graph, ring<string, RING_SIZE>* myRing, std::mutex ringMutex) {
+                // ringMutex.lock();
+            cout << "Child PID: " << getpid() << std::endl;
+            // close(fd2); // Close the listening socket in the child process
+            
+            vector <int> input_numbers;
+            char buffer[1024];
+            ssize_t bytes_read = read(fd2, buffer, sizeof(buffer));
+            buffer[bytes_read] = '\0';
+            std::cout << "All message: " << buffer << std::endl;
+            std::stringstream ss(buffer);
+            // Define a vector to store the substrings
+            std::string substring;
+            for (int i=0; i<2;i++) {
+                std::getline(ss, substring, ' ');
+                std::cout << "substring: " << substring << std::endl;
+                input_numbers.push_back(stoi(substring));
+            }
+            
+            for (int num : input_numbers){
+                std::cerr << num << endl; 
+            }
+            
+            // std::this_thread::sleep_for(std::chrono::milliseconds(1000*30));
+            // Find BFS path
+            std::cout << "search in cache" << "\n";
+            std::cout << "myRing=" << *myRing << endl;
+            ringMutex.lock();
+            int containsIndex = contains(*myRing, input_numbers[0], input_numbers[1]);
+            if(containsIndex >= 0){
+                std::cerr << "reading from the cache index " << containsIndex << "\n";
+                string cachedPath = (*myRing)[containsIndex];
+                ssize_t bytes_written = write(fd2, cachedPath.c_str(), cachedPath.size());
+            }
+            else
+            {
+                std::cerr << "calc bfs" << endl;
+                vector<int> bfs_path = graph.BFS(input_numbers[0], input_numbers[1]);
+                std::cerr << "got BFS : " << endl;
+                string step_as_str;
+                string pathToCache;
+                for (size_t i = 0; i < bfs_path.size(); ++i) {
+                    int step = bfs_path[i];
+                    std::cerr << step << endl; 
+                    if (i < bfs_path.size()-1){
+                        step_as_str = std::to_string(step)+ ' ';
+                        pathToCache += step_as_str;
+                        pathToCache += ' ';
+                    }
+                    else {
+                        step_as_str = std::to_string(step)+ '\n';
+                        pathToCache += step_as_str;
+                    }
+                    ssize_t bytes_written = write(fd2, step_as_str.c_str(), step_as_str.size());
+                }
+                if (bfs_path.size()==0){
+                    step_as_str = '\n';
+                    ssize_t bytes_written = write(fd2, step_as_str.c_str(), step_as_str.size());
+                }else{
+                    std::cerr << "ring address: " << &myRing << endl; 
+                    std::cerr << "pushing to ring " << pathToCache<< endl; 
+                    myRing->push_back(pathToCache);
+                    std::cerr << "element 0 " << myRing[0]<< endl; 
+                }
+                
+            }
+            ringMutex.unlock();
+            close(fd2); // Close the client socket in the child process
+
+}
 
 int main(int argc, char **argv) {
     // Graph graph(argv[1]);
     Graph graph("db_toy.csv");
+    // ring<string, RING_SIZE> myRing;
+    ring<string, RING_SIZE>* myRing = new ring<string, RING_SIZE>();
+    std::mutex ringMutex;
+
+    // ring <struct path ,10 > last_ten;
     
     int fd = socket(AF_INET, SOCK_STREAM, 0);
     if (fd == -1) {
@@ -41,50 +145,7 @@ int main(int argc, char **argv) {
             continue;
         }
         std::cout << "got client, try to read: " << endl;
-
-        if (fork() == 0) {
-            cout << "Child PID: " << getpid() << std::endl;
-            close(fd); // Close the listening socket in the child process
-            
-            vector <int> input_numbers;
-            char buffer[1024];
-            ssize_t bytes_read = read(fd2, buffer, sizeof(buffer));
-            buffer[bytes_read] = '\0';
-            std::cout << "All message: " << buffer << std::endl;
-            std::stringstream ss(buffer);
-            // Define a vector to store the substrings
-            std::string substring;
-            for (int i=0; i<2;i++) {
-                std::getline(ss, substring, ' ');
-                std::cout << "substring: " << substring << std::endl;
-                input_numbers.push_back(stoi(substring));
-            }
-            
-            for (int num : input_numbers){
-                std::cerr << num << endl; 
-            }
-            // Find BFS path
-            vector<int> bfs_path = graph.BFS(input_numbers[0], input_numbers[1]);
-            std::cerr << "got BFS : " << endl;
-            string step_as_str;
-            for (size_t i = 0; i < bfs_path.size(); ++i) {
-                int step = bfs_path[i];
-                std::cerr << step << endl; 
-                if (i < bfs_path.size()-1){
-                step_as_str = std::to_string(step)+ ' ';
-                }
-                else {
-                    step_as_str = std::to_string(step)+ '\n';
-                }
-                ssize_t bytes_written = write(fd2, step_as_str.c_str(), step_as_str.size());
-            }
-            if (bfs_path.size()==0){
-                step_as_str = '\n';
-                ssize_t bytes_written = write(fd2, step_as_str.c_str(), step_as_str.size());
-            }
-            close(fd2); // Close the client socket in the child process
-            return 0;
-        }
+        std::thread handleRequest(fd2, graph, myRing, ringMutex);
         std::cerr << "finish with client" << endl; 
         close(fd2); // Close the client socket in the parent process
     }
